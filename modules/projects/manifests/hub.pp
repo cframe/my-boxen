d99091a97028ffb2899728a7015b96bc914f97f8
Colin <colin@colinframe.com>
2014-02-11 11:28:20 +0000

    * added imagemagick as a dependency to hub

--- modules/projects/manifests/hub.pp
+++ modules/projects/manifests/hub.pp
@@ -1,6 +1,9 @@
 class projects::hub {
   $home = "/Users/${::boxen_user}"
   
+	include libtool
+  include imagemagick
+  
   boxen::project { 'hub':
     dir           => "${home}/Projects/hub", 
     mysql         => true,
