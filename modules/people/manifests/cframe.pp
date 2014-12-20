03c5bbb54111e1817fed28b67e89f5e9abf535fa
Colin <colin@colinframe.com>
2014-12-20 12:10:46 +0000

    * moving all my personal config into a personal manifest, where it should be

---
+++ modules/people/manifests/cframe.pp
@@ -0,0 +1,60 @@
+class people::cframe {
+  # custom modules
+  include firefox
+  include chrome
+  include textmate::textmate2::release
+  include vlc
+  include mysql
+  include gitx
+  # include daisy_disk
+  include dropbox
+  # include appcleaner
+  # include fluid
+  include transmission
+  include wget
+  include googleearth
+  include virtualbox
+  include heroku
+  include sequel_pro
+  include spotify
+  include openoffice
+  include unarchiver
+  include ios7_screensaver
+  include plistfiles
+
+  include osx::global::enable_keyboard_control_access
+  include osx::dock::dim_hidden_apps
+  include osx::finder::show_all_on_desktop
+  include osx::no_network_dsstores
+  include osx::global::key_repeat_rate  
+  include osx::disable_dashboard
+  class { 'osx::dock::icon_size': 
+    size => 31
+  }
+  class { 'osx::dock::zoom_icon_size': 
+    size => 46
+  }
+  class { 'osx::global::key_repeat_delay':
+    delay => 10
+  }
+  include osx::dock::align_left
+  include osx::dock::top_right_hot_corner_screensaver
+
+  git::config::global { 'user.email':
+    value  => 'colin@colinframe.com'
+  }
+  git::config::global { 'user.name':
+    value  => 'Colin'
+  }  
+
+
+  osx::recovery_message { 'If this Mac is found, please call 07980241415': }
+  include garmin_ant_agent
+  include googledrive
+  include trainingpeaks_device_agent
+  include trainerroad
+  include flux
+
+  # include projects::office
+  # include projects::itison
+}
